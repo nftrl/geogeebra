505774e7124900fb886f5cd6cf715b04e5691b35
squidallah <marcuslarsen97@gmail.com>
2020-02-05 20:06:47 +0100

    fixed shit

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, bash, unzip, ... }:
+{ stdenv, fetchurl, bash, unzip, autopatchelf, pkgs, ... }:
 
 stdenv.mkDerivation rec {
   version = "0.1001";
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "0kw8sdsg5af7cz0h0qiyh170h4jqbnq3jb47ba96mj9141z2siir";
   };
 
-  buildInputs = [ bash unzip ];
+  buildInputs = [ bash unzip autopatchelf ];
 
   unpackPhase = ''
     unzip ${src}
@@ -17,9 +17,17 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/bin
+
     cp -R GeoGebra-linux-x64/* $out
     cd $out
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" GeoGebra
+
+#    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" GeoGebra
+#    patchelf --remove-needed libgtk-x11-2.0.so.0 GeoGebra
+#    patchelf --add-needed ${pkgs.gtk2-x11}/lib/libgtk-x11-2.0.so.0 GeoGebra
+#    patchelf --add-needed ${pkgs.gtk2-x11}/lib/libgdk-x11-2.0.so.0 GeoGebra
+#
+    autopatchelf $out/bin
+
     ln -s -T $out/GeoGebra bin/geogebra
   '';
 }
